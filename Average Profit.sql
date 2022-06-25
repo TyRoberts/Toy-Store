1211f4aa0ff01457148ccbb7578a3719c9ac2fe6
TyRoberts <tyrellaroberts@gmail.com>
2022-06-25 13:59:45 -0400

    Create Average Profit
    Displays average profit for each product category and all products.

---
+++ Average Profit.sql
@@ -0,0 +1,13 @@
+SELECT 
+	CASE WHEN 
+		product_category IS NULL 
+		THEN 'All'
+		ELSE product_category 
+	END,
+	ROUND(AVG(product_price - product_cost),2) AS avg_profit
+FROM 
+	products
+GROUP BY 
+	ROLLUP(product_category)
+ORDER BY 
+	product_category;
