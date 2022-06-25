6a1c616562555a0e8c654900eb9e56f0ab0ce5c3
TyRoberts <tyrellaroberts@gmail.com>
2022-06-25 15:27:36 -0400

    Create Times in Top 3
    Displays the number of times each store was in the top 3 based on the number of transactions each quarter.

---
+++ Times in Top 3.sql
@@ -0,0 +1,41 @@
+WITH transactions AS (
+		SELECT 
+			store_name,
+			DATE_PART('quarter',"date") AS "quarter", 
+			DATE_PART('year',"date") AS "year", 
+			COUNT(sale_id) AS total 
+		FROM 
+			sales 
+		INNER JOIN 
+			stores 
+			ON
+			sales.store_id = stores.store_id
+		GROUP BY 
+			store_name,
+			"quarter",
+			"year"),
+	"rank" AS(
+		SELECT 
+		store_name,
+		"year", 
+		"quarter", 
+		SUM(total) AS total_transactions,
+		RANK() OVER(PARTITION BY "quarter", "year" ORDER BY SUM(total) DESC)
+	FROM 
+		transactions
+	GROUP BY 
+		store_name,
+		"year",
+		"quarter")
+
+SELECT 
+	store_name, 
+	COUNT(rank)
+FROM
+	"rank"
+WHERE
+	rank <=3
+GROUP BY 
+	store_name
+ORDER BY 
+	COUNT DESC;
