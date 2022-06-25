07cb7b23110d652c3d1f151ecc0c7d74a03174f1
TyRoberts <tyrellaroberts@gmail.com>
2022-06-25 16:05:32 -0400

    Create Stores per City
    Displays total number of stores and number of stores in each city.

---
+++ Stores per City.sql
@@ -0,0 +1,13 @@
+SELECT 
+	CASE WHEN 
+		store_city IS NULL 
+		THEN 'Total'
+		ELSE store_city 
+	END,
+	COUNT(*) AS num_of_stores 
+FROM 
+	stores
+GROUP BY 
+	ROLLUP(store_city)
+ORDER BY 
+	num_of_stores DESC;
