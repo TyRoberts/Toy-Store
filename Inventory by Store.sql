4608e93de83112e7fb865afe39a643d515f6020d
TyRoberts <tyrellaroberts@gmail.com>
2022-06-25 16:02:41 -0400

    Create Inventory by Store
    For each product, displays stock on hand at each store.

---
+++ Inventory by Store.sql
@@ -0,0 +1,20 @@
+SELECT 
+	store_name, 
+	product_name, 
+	SUM(stock_on_hand) AS stock 
+FROM 
+	inventory
+INNER JOIN 
+	stores 
+	ON
+	inventory.store_id = stores.store_id
+INNER JOIN 
+	products 
+	ON
+	inventory.product_id = products.product_id
+GROUP BY 
+	store_name, 
+	product_name
+ORDER BY 
+	product_name, 
+	store_name;
